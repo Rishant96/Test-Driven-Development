3548a20cd312cae22e9635d660619237e43ef399
Rishant Dutt <rishant.dutt96@gmail.com>
2019-02-24 13:41:34 +0530

    Chrome support added.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,5 +1,8 @@
 from django.urls import reverse
 from django.test import TestCase
+from django.http import HttpRequest
+
+import lists.views as lists_views
 
 
 class HomePageTest(TestCase):
@@ -7,3 +10,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = reverse('lists:home')
         self.assertEqual(found, '/')
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = lists_views.home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do Lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))

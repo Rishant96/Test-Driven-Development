78cd062d787a7281dc3a28986f1e67a667531321
Rishant Dutt <rishant.dutt96@gmail.com>
2019-02-26 09:26:56 +0530

    refactored table check for list items.

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -3,8 +3,6 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    print(request.POST)
-    print(request.POST.get('item_next', 'Not Found'))
     return render(request, 'lists/home.html', {
         'new_item': request.POST.get('item_next', ''),
     })
